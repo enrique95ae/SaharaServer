6ec9fea0aab05a9ee1c15a2b8b3fc4cbe68993b3
enrique95ae <enrique95ae@gmail.com>
2018-11-07 20:01:57 -0800

    databaseManager

--- saharaServer/DatabaseManager.cs
+++ saharaServer/DatabaseManager.cs
@@ -1,1 +1,76 @@
-﻿
+﻿using System;
+using System.Data.SQLite;
+using Dapper;
+using System.Linq;
+
+namespace SaharaServer
+{
+    public class DatabaseManager : BaseSingleton<DatabaseManager>
+    {
+        private const string            _dbSource = "XXXXXXXXXXXXXXXXXX";
+        private readonly Random rng               = new Random();
+
+        public bool CreateAccount(string email, string password)
+        {
+            int numRowsChanged = 0;
+
+            string tag = GeneratedUserTag();
+
+            string sqlInsert = $"insert into AccountData (Tag, Email, Password) values('{tag}', '{email}', '{password}')";
+
+            using (var connection = new SQLiteConnection("XXXXXXXXXXXXX"))
+            {
+                connection.Open();
+
+                try
+                {
+                    numRowsChanged = connection.Execute(sqlInsert);
+                }
+                catch(SQLiteException e)
+                {
+                    Console.WriteLine("\n Database: ERROR occured while inserting into DB\n");
+                    Console.WriteLine(e);
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public AccountData GetAccountData(string email)
+        {
+            if (String.IsNullOrWhiteSpace(email))
+            {
+                return null;
+            }
+
+            string sqlQuery = $"select * from AccountData where Email='{email}";
+
+            using (var connection = new SQLiteConnection("XXXXXXXXXXXXXXXX"))
+            {
+                try
+                {
+                    var accountData = connection.QuerySingle<AccountData>(sqlQuery);
+
+                    if(accountData != null)
+                    {
+                        return accountData;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Database: ERROR selecting user");
+                    Console.WriteLine(e);
+                }
+                return null;
+            }
+        }
+
+        private string GenerateUserTag()
+        {
+            string tag = rng.Next(0, 9999).ToString("D4");
+
+            return tag;
+        }
+    }
+}
+
--- saharaServer/ServerManager.cs
+++ saharaServer/ServerManager.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace SaharaServer
+{
+    public class ServerManager : BaseSingleton<ServerManager>
+    {
+        public List<UserConnection> Connections { get; set; }
+
+        private ServerManager()
+        {
+            Connections = new List<UserConnection>();
+        }
+
+        public void AddConnection(UserConnection newConnection)
+        {
+            Connections.Add(newConnection);
+        }
+
+        public UserData FindUser(string username)
+        {
+            var userData = Connections.Find(user => user.GetUserData.AccountData.UserName, Equals(username));
+            return userData.GetUserData;
+        }
+    }
+}
