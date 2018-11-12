b9501aa29eadb1d1c000729bec6e7551fcf21959
enrique95ae <enrique95ae@gmail.com>
2018-11-12 14:30:46 -0800

    removed line

--- saharaServer/SaharaLib/BaseEvent.cs
+++ saharaServer/SaharaLib/BaseEvent.cs
@@ -22,11 +22,10 @@ namespace SaharaLib
     [ProtoInclude(53, typeof(UserData))]
     [ProtoInclude(54, typeof(ItemData))]
     [ProtoInclude(55, typeof(BillingInfo))]
-<<<<<<< HEAD
 
-=======
+
     [ProtoInclude(56, typeof(PaymentInfo))]
->>>>>>> 65a0f6526a849d2a2ba5d664e2a39a53cea293a8
+
     public class BaseEvent
     {
         [ProtoMember(1)]
